736c1d2a9a046a2fba9bfe86c240b3e1720924b3
Max Cloutier <mcloutier13@gmail.com>
2022-07-21 11:24:35 -0400

    Fix author

--- Mods/CGQC/CGQC/cfgPatches.hpp
+++ Mods/CGQC/CGQC/cfgPatches.hpp
@@ -2,7 +2,7 @@
 class cgqc_patch_logo_ukr
 {
 	displayName = "CGQC Logo Ukraine";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_logo_new.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_logo_new.paa";
 };
@@ -10,7 +10,7 @@ class cgqc_patch_logo_ukr
 class cgqc_patch_logo_black
 {
 	displayName = "CGQC Logo (Blk)";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_logo_black.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_logo_black.paa";
 };
@@ -18,7 +18,7 @@ class cgqc_patch_logo_black
 class cgqc_patch_logo
 {
 	displayName = "CGQC Logo";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_logo.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_logo.paa";
 };
@@ -26,7 +26,7 @@ class cgqc_patch_logo
 class cgqc_patch_logo_camo
 {
 	displayName = "CGQC Logo (camo)";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_logo_camo.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_logo_camo.paa";
 };
@@ -35,7 +35,7 @@ class cgqc_patch_logo_camo
 class cgqc_patch_scpqc
 {
 	displayName = "CGQC SCPQC";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_scpqc.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_scpqc.paa";
 };
@@ -44,7 +44,7 @@ class cgqc_patch_scpqc
 class cgqc_patch_tremblay
 {
 	displayName = "CGQC Tremblay";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_tremblay.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_tremblay.paa";
 };
@@ -52,7 +52,7 @@ class cgqc_patch_tremblay
 class cgqc_patch_frechette
 {
 	displayName = "CGQC Frechette";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_frechette.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_frechette.paa";
 };
@@ -60,7 +60,7 @@ class cgqc_patch_frechette
 class cgqc_patch_comeau
 {
 	displayName = "CGQC Comeau";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_comeau.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_comeau.paa";
 };
@@ -68,7 +68,7 @@ class cgqc_patch_comeau
 class cgqc_patch_fortin
 {
 	displayName = "CGQC Fortin";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_fortin.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_fortin.paa";
 };
@@ -76,7 +76,7 @@ class cgqc_patch_fortin
 class cgqc_patch_laforest
 {
 	displayName = "CGQC Laforest";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_laforest.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_laforest.paa";
 };
@@ -84,7 +84,7 @@ class cgqc_patch_laforest
 class cgqc_patch_pinard
 {
 	displayName = "CGQC Pinard";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_pinard.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_pinard.paa";
 };
@@ -92,7 +92,7 @@ class cgqc_patch_pinard
 class cgqc_patch_villeneuve
 {
 	displayName = "CGQC Villeneuve";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_villeneuve.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_villeneuve.paa";
 };
@@ -100,7 +100,7 @@ class cgqc_patch_villeneuve
 class cgqc_patch_audi
 {
 	displayName = "CGQC Audi";
-	author = "silent1";
+	author = "audi/silent1";
 	texture = "\cgqc\patches\cgqc_patch_audi.paa";
 	textureVehicle = "\cgqc\patches\cgqc_patch_audi.paa";
 };
