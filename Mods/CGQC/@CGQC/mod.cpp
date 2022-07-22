8516d3bfd1f6a054b1f5ccb4af7130356fc4e89e
Max Cloutier <mcloutier13@gmail.com>
2022-07-22 11:30:21 -0400

    Added: better icons/glow

--- Mods/CGQC/@CGQC/mod.cpp
+++ Mods/CGQC/@CGQC/mod.cpp
@@ -1,5 +1,14 @@
 name = "CGQC_Core";
-picture = "mod.paa";
 actionName = "Discord";
 action = "https://discord.gg/NyhmQmej";
-description = "Version: 0.1";
+description = "CGQC Core v0.1";
+picture = "mod.paa";
+
+logo = "cgqc_core_128.paa";
+logoOver = "cgqc_core_glow_128.paa";
+logoSmall = "cgqc_core_32.paa";
+
+tooltip = "CGQC Core";
+tooltipOwned = "CGQC Core - Owned";
+overview = "CGQC<br/><br/>https://discord.gg/THNAHP8H";
+author = "silent1";
